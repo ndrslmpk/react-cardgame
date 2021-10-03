35f34a9e446cf951bc5b1299e6ed63cb9efae3a1
ndrslmpk <andreas.lempik@gmail.com>
2021-10-03 19:52:53 +0200

    added Card and static datatype 'animal'

--- cardgame/src/Card.jsx
+++ cardgame/src/Card.jsx
@@ -1,36 +1,41 @@
 import * as React from 'react';
 
+import './Card.css';
+import Animal from './Animal';
+
 export default function Card() {
+  const elephant = new Animal('Elefant', 'placeholder.png', 3.3, 6000, 70, 1, 40);
+
   return (
     <div className="card">
-      <h1>Elefant</h1>
+      <h1>{elephant.name}</h1>
       <img
         alt="Elefant"
         height="200"
         width="200"
-        src={`${process.env.PUBLIC_URL}/placeholder.png`}
+        src={`${process.env.PUBLIC_URL}/${elephant.image}`}
       />
       <table>
         <tbody>
           <tr>
             <td>Größe</td>
-            <td>3.3 m</td>
+            <td>{elephant.size} m</td>
           </tr>
           <tr>
             <td>Gewicht</td>
-            <td>6000 kg</td>
+            <td>{elephant.weight} kg</td>
           </tr>
           <tr>
             <td>Alter</td>
-            <td>70 Jahre</td>
+            <td>{elephant.age} Jahre</td>
           </tr>
           <tr>
             <td>Nachkommen</td>
-            <td>1</td>
+            <td>{elephant.offspring}</td>
           </tr>
           <tr>
             <td>Geschwindigkeit</td>
-            <td>40 km</td>
+            <td>{elephant.speed} km</td>
           </tr>
         </tbody>
       </table>
